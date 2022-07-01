3cc54ba9d601bd4425e6c277bb6bae2065577d97
Петров Слава <pvv56@mail.ru>
2022-07-01 17:32:48 +0300

    Созвон

--- js/util.js
+++ js/util.js
@@ -1,24 +1,3 @@
-import { getRandomPositiveInteger } from './random-functions.js';
-
-const getRandomElements = (array) => {
-  const randomInt = getRandomPositiveInteger(1, array.length);
-  const elements = [];
-
-  while (elements.length < randomInt) {
-    const index = getRandomPositiveInteger(0, array.length - 1);
-    const element = array[index];
-
-    if (!elements.includes(element)) {
-      elements.push(element);
-    }
-  }
-
-  return elements;
-};
-
-const getRandomArrayElement = (elements) =>
-  elements[getRandomPositiveInteger(0, elements.length - 1)];
-
 const createNumbers = () => {
   const numbers = [];
   for (let i = 0; i < 10; i++) {
