71603ddb7cb558d07e08729cda800ad878712876
Петров Слава <pvv56@mail.ru>
2022-07-23 21:53:23 +0300

    dry резета

--- js/reset-forms.js
+++ js/reset-forms.js
@@ -1,5 +1,9 @@
 import { filterForm } from './activate-page.js';
 import {noticeForm} from './form-validation.js';
 
-export const resetFilterForm = () => filterForm.reset();
-export const resetNoticeForm = () => noticeForm.reset();
+const resetForm = (form) => {
+  form.reset();
+}
+
+export const resetFilterForm = () => resetForm(filterForm);
+export const resetNoticeForm = () => resetForm(noticeForm);
