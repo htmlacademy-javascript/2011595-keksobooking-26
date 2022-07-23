bd9859c36c740990f5c889d750effe340d65240f
Петров Слава <pvv56@mail.ru>
2022-07-23 06:56:56 +0300

    удалил2

--- js/main.js
+++ js/main.js
@@ -18,8 +18,6 @@ if (condition) {
     }
   );
   // Перевод страницы в активное состояние
-  // changeFormsState(true);
   // Вносить изменения в форму и отправлять её на сервер;
-  // noticeForm.addEventListener('submit', onNoticeFormSubmit);
   // Фильтрация.
 }
